fadb9f2e916c56287461b009227d02accd3af872
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2021-10-09 16:58:34 -0300

    update script to support Mint 20.2

--- README.md
+++ README.md
@@ -1,27 +1,44 @@
 # Setup Linux Pescar
 
-> This script sets up the Projeto Pescar Procempa's machines in order to avoid the students to do that in their first class.
+Script to setup the system used during the class
 
-> Customized for Linux Mint 18
+> Customized for Linux Mint 20.2
+
+
+### When testing a new Mint version
+
+**Backup firefox profile:**
+
 
 ```bash
-# This script should:
-# - Set HTTP_PROXY variables.
-# - Set apt-get proxy.
-# - Install Open SSH Server.
-# - Update/Install some packages.
-# - Update Firefox profile.
-#
-#              CHANGE HISTORY
-# 3.4 (10 Jun 2018) - Changes menu options order to allow the system update run first
-# 3.3 (12 Jun 2017) - Changes license from GPL to MIT. Changed size of Zenity window
-# 3.2 (11 Jun 2017) - Improving log messages and updating script to support Mint 18
-# 3.1.1 (05 Jun 2016) - Removed Proxy information (in order to public the source)
-# 3.1 (05 Jun 2016) - Added again the functions to set proxy to Firefox and Chormium. Added Zenity to display the options sbox
-# 3.0 (25 May 2016) - Refectored all functions. Removed functions to set proxy to Firefox and Chormium
-# 2.0 (20 May 2015) - Recreated the functions to set proxy to browsers and added its options to menu
-# 1.3 (15 May 2015) - Fixed double execution error when executing sudo via 'sudo -S'
-# 1.2 (14 May 2015) - Fixed error in some functions and improved them as well
-# 1.1 (14 May 2015) - Added dialog as default UI
-# 1.0 (08 Jun 2014) - First version
+$ cd /home/pescar/.mozilla/firefox/
+$ mv <profile> 'default_pescar_profile'
+$ tar zcvf firefox_profile.tar.gz default_pescar_profile
+```
+
+
+**Packages to remember to install when testing a new Linux version**
+
+```
+openssh-client
+openssh-server
+openssh-sftp-server
+ssh
+vlc
+chromium
+vim
+htop
+cowsay
+sl
+fortunes-br
+xgalaga
+gnome-chess
+lbreakout2
+gimp
+```
+
+**Backup apt cache**
+```bash
+$ cd /var/cache
+$ tar cvf apt.tar apt
 ```
--- setup_pescar_machine.sh
+++ setup_pescar_machine.sh
@@ -21,18 +21,6 @@
 # - Update/Install some packages.
 # - Update Firefox profile.
 #
-#              CHANGE HISTORY
-# 3.4 (10 Jun 2018) - Changes menu options order to allow the system update run first
-# 3.3 (12 Jun 2017) - Changes license from GPL to MIT. Changed size of Zenity window
-# 3.2 (11 Jun 2017) - Improving log messages and updating script to support Mint 18
-# 3.1.1 (05 Jun 2016) - Removed Proxy information (in order to public the source)
-# 3.1 (05 Jun 2016) - Added again the functions to set proxy to Firefox and Chormium. Added Zenity to display the options sbox
-# 3.0 (25 May 2016) - Refectored all functions. Removed functions to set proxy to Firefox and Chormium
-# 2.0 (20 May 2015) - Recreated the functions to set proxy to browsers and added its options to menu
-# 1.3 (15 May 2015) - Fixed double execution error when executing sudo via 'sudo -S'
-# 1.2 (14 May 2015) - Fixed error in some functions and improved them as well
-# 1.1 (14 May 2015) - Added dialog as default UI
-# 1.0 (08 Jun 2014) - First version
 #
 # TODO (jonathadv): Define a better way to present messages, choose one lang only.
 #
@@ -321,23 +309,37 @@ function configure_firefox_profile(){
     local default_pescar_profile_dir='default_pescar_profile'
     local current_firefox_profile_name=''
 
+    if [[ ! -d "${firefox_profile_basedir}" ]]; then
+        logger "directory ${firefox_profile_basedir} does not exist. Starting and stopping firefox to create it!"
+        
+        sudo -u pescar firefox 2> /dev/null &
+        sleep 5
+
+        while ! pidof firefox; do
+            logger "Waiting firefox"
+            sleep 1
+        done
+
+        sleep 5
+        killall firefox
+    fi
+
     cd "${BASE_DIR}"
 
     print_header 'Configure Firefox profile'
 
-    if ls -d ${firefox_profile_basedir}/*.default 2> /dev/null; then
-        current_firefox_profile_name=$(basename $(ls -d ${firefox_profile_basedir}/*.default))
-    else
-        logger "Unable to find any profile folder under \"${firefox_profile_basedir}\". Skipping profile changes."
-        return 1
-    fi
+    if ls -d ${firefox_profile_basedir}/*.default-* 2> /dev/null; then
+        current_firefox_profile_name=$(basename $(ls -d ${firefox_profile_basedir}/*.default-*))
+        backup "${firefox_profile_basedir}/${current_firefox_profile_name}"
+        rm -rf "${firefox_profile_basedir}/${current_firefox_profile_name}"
 
-    backup "${firefox_profile_basedir}"
+    else
+        logger "Unable to find any profile folder under \"${firefox_profile_basedir}\". No need to backup."
+    fi
 
     if cp -f  "${BASE_DIR}/${tar_file}" "${firefox_profile_basedir}"; then
         cd "${firefox_profile_basedir}"
         tar -zxf "${tar_file}"
-        rm -rf "${current_firefox_profile_name}"
 
         mv "${default_pescar_profile_dir}" "${current_firefox_profile_name}"
         chown -R pescar.pescar "${current_firefox_profile_name}"
@@ -444,7 +446,7 @@ export NO_PROXY='${NO_PROXY}'"  >> "${dest_file}"
 #   None
 #######################################
 function set_proxy_to_chromium(){
-  local chromium="$(which chromium-browser)"
+  local chromium="$(which chromium)"
   local chromium_bin="${chromium}-bin"
 
   print_header 'Set proxy to Chromium'
@@ -641,7 +643,7 @@ function start_set_up(){
             --checklist  \
             --column "[X]" \
             --column "${DIALOG_OPTIONS}" \
-            FALSE "${DIALOG_OPT_UPDATE}" \
+            TRUE "${DIALOG_OPT_UPDATE}" \
             TRUE "${DIALOG_OPT_PROXY_SYSTEM}" \
             TRUE "${DIALOG_OPT_PROXY_APT}" \
             TRUE "${DIALOG_OPT_BROWSER_PROXY}" \
